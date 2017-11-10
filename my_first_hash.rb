6dde101fceb813dead9f07d2c3ffae0834318d09
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-10 20:30:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {"puppy" => "finn", "black_cat" => "george", "orange_cat" => "precious"}
   
 end
 
